f0a755bbba80ef90dc3310c9f547183974036b2f
Americool <aberanderson@gmail.com>
2017-03-17 15:12:15 -0400

    some degree of working

--- src/components/EntryEditor/EntryEditor.js
+++ src/components/EntryEditor/EntryEditor.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import SplitPane from 'react-split-pane';
+import Button from 'react-toolbox/lib/button';
 import { ScrollSync, ScrollSyncPane } from '../ScrollSync';
 import ControlPane from '../ControlPanel/ControlPane';
 import PreviewPane from '../PreviewPane/PreviewPane';
@@ -10,6 +11,7 @@ import styles from './EntryEditor.css';
 class EntryEditor extends Component {
   state = {
     showEventBlocker: false,
+    previewOpen: true,
   };
 
   handleSplitPaneDragStart = () => {
@@ -24,7 +26,65 @@ class EntryEditor extends Component {
     this.controlPaneRef.validate();
     this.props.onPersist();
   };
+  handleOpenClosePreview = () => {
+    if (this.state.previewOpen) {
+      this.setState({previewOpen: false})
+    }
+    else {
+      this.setState({previewOpen: true})
+    }
+  };
 
+  showOnlyEditor = () => {
+    const {
+        collection,
+        entry,
+        fields,
+        fieldsMetaData,
+        fieldsErrors,
+        getAsset,
+        onChange,
+        onValidate,
+        onAddAsset,
+        onRemoveAsset,
+        onCancelEdit,
+    } = this.props;
+    const controlClassName = `${ styles.controlPane } ${ this.state.showEventBlocker && styles.blocker }`;
+    return (
+      <div className={styles.root}>
+        <ScrollSync>
+          <div className={styles.container}>
+            <ScrollSyncPane>
+              <div className={controlClassName}>
+                  <Button onClick={this.handleOpenClosePreview.bind(this)}> OPEN </Button>
+                <ControlPane
+                  collection={collection}
+                  entry={entry}
+                  fields={fields}
+                  fieldsMetaData={fieldsMetaData}
+                  fieldsErrors={fieldsErrors}
+                  getAsset={getAsset}
+                  onChange={onChange}
+                  onValidate={onValidate}
+                  onAddAsset={onAddAsset}
+                  onRemoveAsset={onRemoveAsset}
+                  ref={c => this.controlPaneRef = c} // eslint-disable-line
+                />
+              </div>
+            </ScrollSyncPane>
+          </div>
+        </ ScrollSync>
+        <div className={styles.footer}>
+          <Toolbar
+            isPersisting={entry.get('isPersisting')}
+            onPersist={this.handleOnPersist}
+            onCancelEdit={onCancelEdit}
+          />
+        </div>
+
+      </div>
+    );
+  }
   render() {
     const {
         collection,
@@ -39,9 +99,12 @@ class EntryEditor extends Component {
         onRemoveAsset,
         onCancelEdit,
     } = this.props;
-
     const controlClassName = `${ styles.controlPane } ${ this.state.showEventBlocker && styles.blocker }`;
     const previewClassName = `${ styles.previewPane } ${ this.state.showEventBlocker && styles.blocker }`;
+
+    if (!this.state.previewOpen) {
+      return this.showOnlyEditor();
+    }
     return (
       <div className={styles.root}>
         <ScrollSync>
@@ -53,7 +116,6 @@ class EntryEditor extends Component {
             >
               <ScrollSyncPane>
                 <div className={controlClassName}>
-
                   <ControlPane
                     collection={collection}
                     entry={entry}
@@ -67,10 +129,10 @@ class EntryEditor extends Component {
                     onRemoveAsset={onRemoveAsset}
                     ref={c => this.controlPaneRef = c} // eslint-disable-line
                   />
-
                 </div>
               </ScrollSyncPane>
               <div className={previewClassName}>
+                <Button onClick={this.handleOpenClosePreview.bind(this)}> CLOSE </Button>
                 <PreviewPane
                   collection={collection}
                   entry={entry}

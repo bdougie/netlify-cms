9cfc0a2ab873079ad1bfb016a5c79f8c1d65af4c
Americool <aberanderson@gmail.com>
2017-03-24 12:45:01 -0400

    made const changes, struggling to get localStorage to work

--- src/components/EntryEditor/EntryEditor.js
+++ src/components/EntryEditor/EntryEditor.js
@@ -1,14 +1,23 @@
 import React, { Component, PropTypes } from 'react';
 import ImmutablePropTypes from 'react-immutable-proptypes';
+import SplitPane from 'react-split-pane';
+import Button from 'react-toolbox/lib/button';
+import { ScrollSync, ScrollSyncPane } from '../ScrollSync';
+import ControlPane from '../ControlPanel/ControlPane';
+import PreviewPane from '../PreviewPane/PreviewPane';
+import Toolbar from './EntryEditorToolbar';
 import styles from './EntryEditor.css';
-import ShowOnlyEditor from './ShowOnlyEditor';
-import ShowEditorWithPreview from './ShowEditorWithPreview';
+
+const PREVIEW_STATE = 'cms.preview-state';
 
 class EntryEditor extends Component {
-  state = {
-    showEventBlocker: false,
-    previewOpen: true,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      showEventBlocker: false,
+      previewOpen: (localStorage.getItem(PREVIEW_STATE) !== null ? localStorage.getItem(PREVIEW_STATE) : true),
+    };
+  }
 
   handleSplitPaneDragStart = () => {
     this.setState({ showEventBlocker: true });
@@ -25,33 +34,82 @@ class EntryEditor extends Component {
 
   handleTogglePreview = () => {
     const { previewOpen } = this.state;
-    this.setState({ previewOpen: !previewOpen });
+    this.setState({ previewOpen: !previewOpen }, localStorage.setItem(PREVIEW_STATE, !previewOpen));
   };
 
   render() {
+    const {
+        collection,
+        entry,
+        fields,
+        fieldsMetaData,
+        fieldsErrors,
+        getAsset,
+        onChange,
+        onValidate,
+        onAddAsset,
+        onRemoveAsset,
+        onCancelEdit,
+    } = this.props;
+
     const controlClassName = `${ styles.controlPane } ${ this.state.showEventBlocker && styles.blocker }`;
     const previewClassName = `${ styles.previewPane } ${ this.state.showEventBlocker && styles.blocker }`;
 
-    return this.state.previewOpen ?
-      <ShowEditorWithPreview
-        {...this.props}
-        styles={styles}
-        handleOnPersist={this.handleOnPersist}
-        handleTogglePreview={this.handleTogglePreview}
-        handleSplitPaneDragStart={this.handleSplitPaneDragStart}
-        handleSplitPaneDragFinished={this.handleSplitPaneDragFinished}
-        controlClassName={controlClassName}
-        previewClassName={previewClassName}
-        controlPaneRef={this.controlPaneRef}
-      />
-    : <ShowOnlyEditor
-      {...this.props}
-      styles={styles}
-      handleOnPersist={this.handleOnPersist}
-      handleTogglePreview={this.handleTogglePreview}
-      controlClassName={controlClassName}
-      controlPaneRef={this.controlPaneRef}
-    />;
+    const editor = (
+      <div className={controlClassName}>
+        <Button style={{ 'margin-left': '35vw' }} onClick={this.handleTogglePreview}> TOGGLE PREVIEW </Button>
+        <ControlPane
+          collection={collection}
+          entry={entry}
+          fields={fields}
+          fieldsMetaData={fieldsMetaData}
+          fieldsErrors={fieldsErrors}
+          getAsset={getAsset}
+          onChange={onChange}
+          onValidate={onValidate}
+          onAddAsset={onAddAsset}
+          onRemoveAsset={onRemoveAsset}
+          ref={c => this.controlPaneRef = c} // eslint-disable-line
+        />
+      </div>
+    );
+
+    const editorWithPreivew = (
+      <ScrollSync>
+        <div className={styles.container}>
+          <SplitPane
+            defaultSize="50%"
+            onDragStarted={this.handleSplitPaneDragStart}
+            onDragFinished={this.handleSplitPaneDragFinished}
+          >
+            <ScrollSyncPane>
+              {editor}
+            </ScrollSyncPane>
+            <div className={previewClassName}>
+              <PreviewPane
+                collection={collection}
+                entry={entry}
+                fields={fields}
+                fieldsMetaData={fieldsMetaData}
+                getAsset={getAsset}
+              />
+            </div>
+          </SplitPane>
+        </div>
+      </ScrollSync>
+    );
+    return (
+      <div className={styles.root}>
+        {this.state.previewOpen ? editorWithPreivew : editor}
+        <div className={styles.footer}>
+          <Toolbar
+            isPersisting={entry.get('isPersisting')}
+            onPersist={this.handleOnPersist}
+            onCancelEdit={onCancelEdit}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
